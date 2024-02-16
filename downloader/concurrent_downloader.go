a80514f929eeb1dcf9df9c5d520ed8f38dcb5142
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2024-02-17 00:38:16 +0530

    solved overlapping bit issue
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- downloader/concurrent_downloader.go
+++ downloader/concurrent_downloader.go
@@ -2,25 +2,11 @@ package downloader
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"os"
-	"sync"
 )
 
 func DownloadFileConcurrently(url, outputPath string, numWorkers int) error {
-	resp, err := http.Head(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("HTTP response error: %s", resp.Status)
-	}
-
-	fileSize := resp.ContentLength
-	fmt.Printf("File size: %d bytes\n", fileSize)
 
 	file, err := os.Create(outputPath)
 	if err != nil {
@@ -28,63 +14,55 @@ func DownloadFileConcurrently(url, outputPath string, numWorkers int) error {
 	}
 	defer file.Close()
 
-	var wg sync.WaitGroup
-	chunkSize := fileSize / int64(numWorkers)
+	resp, err := http.Head(url)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("HTTP response code error: %s", resp.StatusCode)
+	}
+
+	fileSize := resp.ContentLength
+
+	fetchContentSize := fileSize / int64(numWorkers)
+
+	var start, end int64
 
 	for i := 0; i < numWorkers; i++ {
-		start := int64(i) * chunkSize
-		end := start + chunkSize
+		if i == 0 {
+			start = fetchContentSize * int64(i)
+		} else {
+			start = end + 1
+		}
+		end = start + fetchContentSize
 		if i == numWorkers-1 {
-			// Last worker may download slightly more if not divisible evenly
 			end = fileSize
 		}
-
-		wg.Add(1)
 		go func(start, end int64) {
-			defer wg.Done()
 			err := downloadChunk(url, file, start, end)
 			if err != nil {
 				fmt.Println("Error:", err)
 			}
 		}(start, end)
 	}
-
-	wg.Wait()
-
-	fmt.Println("Download complete")
 	return nil
 }
 
-func downloadChunk(url string, file *os.File, start, end int64) error {
+func downloadChunk(url string, file *os.File, start int64, end int64) error {
 	client := http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return err
-	}
-
-	// Specify the range of bytes to download
-	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, end))
-
+	req, _ := http.NewRequest("GET", url, nil)
+	rangeHeader := fmt.Sprintf("bytes=%d-%d", start, end)
+	req.Header.Set("Range", rangeHeader)
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
+	if resp.StatusCode != http.StatusPartialContent {
+
+	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusPartialContent {
-		return fmt.Errorf("HTTP response error: %s", resp.Status)
-	}
-
-	_, err = file.Seek(start, io.SeekStart)
-	if err != nil {
-		return err
-	}
-
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Downloaded chunk: %d-%d\n", start, end)
 	return nil
+
 }
